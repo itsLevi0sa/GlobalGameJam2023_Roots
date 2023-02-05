44b8c0176943fd366eccc24e01a569d8a49b1ac6
elli <56318580+itsLevi0sa@users.noreply.github.com>
2023-02-05 13:21:50 +0200

    ui setup

---
+++ GGJ_2023_Roots/Assets/BagTableArea.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BagTableArea : MonoBehaviour
+{
+    public GameObject bagPickupAnimator;
+    public GameObject bagHighlight;
+
+    private void Start()
+    {
+        bagPickupAnimator.GetComponent<Animator>().enabled = false;
+        bagHighlight.GetComponent<Animator>().enabled = false;
+        bagHighlight.GetComponent<Image>().enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        bagPickupAnimator.GetComponent<Animator>().enabled = true;
+        bagPickupAnimator.GetComponent<Animator>().Play("BagPickupPoint", - 1, 0.0f);
+        bagHighlight.GetComponent<Image>().enabled = true;
+        bagHighlight.GetComponent<Animator>().enabled = true;
+        bagHighlight.GetComponent<Animator>().Play("BagHighlight", -1, 0.0f);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        bagPickupAnimator.GetComponent<Animator>().enabled = false;
+
+        bagHighlight.GetComponent<Animator>().Play("BagNotHighlighted", -1, 0.0f);
+     
+    }
+}
