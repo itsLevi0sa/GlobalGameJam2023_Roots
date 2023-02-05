d5c2421a56df6c7177d8795d1b415c0c658a8850
Thanos Makris <51239501+thanosmakris@users.noreply.github.com>
2023-02-05 14:17:26 +0200

    sounds

--- GGJ_2023_Roots/Assets/Scripts/CarnivorousPlant.cs
+++ GGJ_2023_Roots/Assets/Scripts/CarnivorousPlant.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 
 public class CarnivorousPlant : MonoBehaviour
 {
+    public AudioSource hitaudioSource;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player1") || other.CompareTag("Player2"))
         {
+            hitaudioSource.Play();
             other.GetComponent<PlayerController>().GetDamage();
             GetComponentInParent<Root>().ReturnToLvl1();
         }
